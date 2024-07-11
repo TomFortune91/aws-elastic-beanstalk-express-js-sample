c3e9b4515deb81af9b235c3d3c5c7f726de2704c
TomFortune91 <fortue22@live.com>
2024-07-11 11:28:19 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Tom!'));
+app.get('/', (req, res) => res.send('Hello Tom Ncube!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
